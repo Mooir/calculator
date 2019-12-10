8141ddfbcc9fcf54099cf842faa4ff017c648e03
王甜甜 <1453637731@qq.com>
2019-12-10 17:40:28 +0800

    等额本金--计算

--- cases/loan/LoanCase.py
+++ cases/loan/LoanCase.py
@@ -15,8 +15,9 @@ class LoanCount(unittest.TestCase):
         mobile_emulation = {'deviceName': 'iPhone X'}
         options = webdriver.ChromeOptions()
         options.add_experimental_option('mobileEmulation', mobile_emulation)
+        # options.set_headless()#浏览器无头模式
         self.driver = webdriver.Chrome(options=options)
-        self.driver.implicitly_wait(5)
+        self.driver.implicitly_wait(10)
         self.url = "https://apps.eshiyun.info/tools/"
         self.loan_page = LoanPage(self.driver,self.url)
         self.result_page = LoanResultPage(self.driver, self.url)
@@ -26,12 +27,24 @@ class LoanCount(unittest.TestCase):
 
     def test_1(self):
         self.loan_page.open()
-        val = self.loan_page.get_value_year()
-        acc_rate = self.loan_page.get_acc_rate()
-        print(acc_rate, type(acc_rate))
+        # val = self.loan_page.get_value_year()
+        # acc_rate = self.loan_page.get_acc_rate()
+        # print(acc_rate, type(acc_rate))
         # self.driver.find_elements_by_class_name()
-        self.loan_page.select_year(25)
-        print(self.loan_page.get_value_year())
+        list = self.loan_page.get_acc_testdata()
+        result = self.loan_page.equal_corpus(30, 10, 0.0325)
+        print(result)
+        for item in list:
+            self.acc_count = str(item['贷款金额'])
+            self.acc_year = str(item['贷款年限'])
+            self.loan_page.input_acc_count(self.acc_count)
+            self.loan_page.select_year(self.acc_year)
+            sleep(0.5)
+            self.loan_page.select_acc_rate()#随机
+            sleep(0.5)
+            self.loan_page.click_count()
+            sleep(1)
+            self.driver.back()
         sleep(5)
 
 
--- pages/LoanPage.py
+++ pages/LoanPage.py
@@ -1,8 +1,10 @@
 import random
 import sys
+import math
+import numpy as np
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-sys.path.append('C:\\code\\calculator\\common')
+sys.path.append('C:\\code\\calculator')
 from common.common import *
 from time import sleep
 
@@ -34,14 +36,19 @@ class LoanPage(BasePages):
     loc_select = "//li[@class='wheel-item']"
     # 选择栏确认按钮
     loc_confer = "//span[@class='pt-submit']"
+    # 测试数据
+    filepath = "C:/code/calculator/cases/loan/TestData.xlsx"
+    sheetname_acc = "Sheet1"
+    sheetname_buss = "Sheet2"
+    sheetname_mix = "Sheet3"
 
     # 打开贷款计算器
     def open(self):
         self._open(self.base_url)
 
     # 产生随机数
-    def randit(self, val1,val2):
-        return random.randint(val1, val2)
+    def randit(self, int1,int2):
+        return random.randint(int1, int2)
 
     # 获取贷款年限文本框内容
     def get_value_year(self):
@@ -58,43 +65,103 @@ class LoanPage(BasePages):
         list = self.get_ele_value(self.driver, self.loc_buss_rate, 'value')
         return float(list[0])
 
+    # 输入贷款公积金金额
+    def input_acc_count(self, keys):
+        self.find_element(*self.loc_input_acc).clear()
+        self.find_element(*self.loc_input_acc).send_keys(keys)
+
+    # 输入商业贷款金额
+    def input_buss_count(self, keys):
+        self.find_element(*self.loc_input_buss).clear()
+        self.find_element(*self.loc_input_buss).send_keys(keys)
+
+    # 点击计算
+    def click_count(self):
+        self.find_element(*self.loc_count).click()
+
     # 选择贷款年限[0-29]
     def select_year(self, year_num):
+        year_num = int(float(year_num))
         # self.find_element(*self.loc_year).click()
         self.driver.find_element_by_xpath(self.loc_year).click()
-        sleep(0.5)
+        sleep(0.2)
         list = self.driver.find_elements_by_xpath(self.loc_select)
         for i in range(0, 31-year_num):
             list[i].click()
             sleep(0.5)
-        sleep(0.5)
+        # sleep(0.5)
         self.driver.find_elements_by_xpath(self.loc_confer)[0].click()
 
     # 选择公积金贷款利率[30-31]
-    def select_acc_rate(self, acc_rate):
+    def select_acc_rate(self):
         self.driver.find_element_by_xpath(self.loc_acc_rate).click()
         sleep(0.5)
         list = self.driver.find_elements_by_xpath(self.loc_select)
-        if acc_rate == '公积金基准利率':
-            list[30].click()
-        elif acc_rate =='二套房固定利率':
-            list[31].click()
+        acc_rate = self.randit(30, 31)
+        list[acc_rate].click
         self.driver.find_elements_by_xpath(self.loc_confer)[1].click()
 
     # 选择商业贷款利率[32-47]
-    def select_buss_rate(self, buss_rate):
+    def select_buss_rate(self):
         self.driver.find_element_by_xpath(self.loc_buss_rate).click()
         sleep(0.5)
         list = self.driver.find_elements_by_xpath(self.loc_select)
-        if buss_rate == '商业贷基准利率':
-            i = 32
-        elif buss_rate == '7折利率':
-            i = 33
-        elif buss_rate == '8折利率':
-            i = 34
-        elif buss_rate == '':
-            i = 35
-
-        # for val in range(i+)
-
+        buss_rate = self.randit(32, 47)
+        for i in range(32, buss_rate):
+            list[i].click
+            sleep(0.5)
         self.driver.find_elements_by_xpath(self.loc_confer)[2].click()
+
+    # 获取公积金贷款测试数据
+    def get_acc_testdata(self):
+        list = self.read_excel(self.filepath, self.sheetname_acc)
+        return list
+    
+    # 获取商业贷款测试数据
+    def get_buss_testdata(self):
+        list = self.read_excel(self.filepath, self.sheetname_buss)
+        return list
+
+    def get_mix_testdata(self):
+        """获取组合贷款测试数据"""
+        list = self.read_excel(self.filepath, self.sheetname_mix)
+        return list
+
+    def repay_equal_capital(self, capital, y_rate, periods):
+        """等额本金"""
+        count = 0
+        periods = 12*periods
+        for i in range(0, periods):
+
+        monthlyPayment[i] = np.around(
+            (mortgage/month)+
+            (
+                (mortgage)*monthlyInterestRate(anualInterestRate)
+            ) - 
+            (i*(mortgage/month)*monthlyInterestRate(anualInterestRate)),2)
+
+
+
+            m_capital = capital/periods             #每月应还本金
+
+            m_repay[i] = np.round((capital/periods)+((capital)))
+        return
+
+    def _month_rate(self, y_rate):
+        """每月利率"""
+        return (y_rate/12)/100
+
+    def _total_rate(self, capital, m_repay, periods)
+        """总利息"""
+        return m_repay*(periosd*12) - capital
+
+    def _equal_capital_rate(self, periods):
+        """比例系数-每月应还"""
+        R = self._month_rate
+        N = periods*12
+        I = R * math.pow(1+R, N)/ (math.pow(1+R, N)-1)
+        return I
+
+
+
+
