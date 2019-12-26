d64daf666633aa74fdcf42b2f1735bcba490ee1b
王甜甜 <1453637731@qq.com>
2019-12-26 17:20:38 +0800

    个税计算器-年终奖计算结果页

--- pages/ResultBounsPage.py
+++ pages/ResultBounsPage.py
@@ -2,6 +2,13 @@ import sys
 sys.path.append("./")
 from common.common import *
 
+"""
+introduction：个税计算器：计算年终奖结果页
+author：黄思梦
+date-last-modified：2019-12-26
+last-modified-by：黄思梦
+"""
+
 
 class BounsTax(BasePages):
     loc_tax = "//*[@id='tax_result']/div[2]/div[15]"
